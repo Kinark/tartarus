cc8ae38eff19f7fc5f63f4ba1b87b917745f2eaf
Kinark <igormarcossi@gmail.com>
2019-03-25 16:02:47 -0300

    Prettier

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import { render } from 'react-dom';
-import App from '~/App';
+import React from 'react'
+import { render } from 'react-dom'
+import App from '~/App'
 import './styles.global.scss'
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom'
 
 const root = document.getElementById('root')
 
-render(<BrowserRouter><Route component={App} /></BrowserRouter>, root);
+render(
+   <BrowserRouter>
+      <Route component={App} />
+   </BrowserRouter>,
+   root
+)
 
 if (module.hot) {
    module.hot.accept()
