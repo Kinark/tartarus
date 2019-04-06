b67dc0a2584fda16e838b08c037c4ff5472ae26f
Kinark <igormarcossi@gmail.com>
2019-04-06 07:39:37 -0300

    Added redux-dev-tools extension

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,12 +1,18 @@
 import { applyMiddleware, createStore } from 'redux'
 import logger from 'redux-logger'
+import { composeWithDevTools } from 'redux-devtools-extension'
+
+import * as actionCreators from '../actions/settings'
 import rootReducer from '../reducers'
 import initialStore from './initialStore'
 
-let middlewares;
+let middlewares
 
 if (process.env.NODE_ENV === 'development') {
-   middlewares = applyMiddleware(logger)
+   const composeEnhancers = composeWithDevTools({
+      actionCreators
+   })
+   middlewares = composeEnhancers(applyMiddleware(logger))
 }
 
 const store = createStore(rootReducer, initialStore, middlewares)
