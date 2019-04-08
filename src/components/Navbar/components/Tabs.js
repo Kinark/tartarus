407ab1e9b64b57b03726d61fd20cf34e9ace9786
Kinark <igormarcossi@gmail.com>
2019-04-08 01:42:56 -0300

    Code optimization

--- src/components/Navbar/components/Tabs.js
+++ src/components/Navbar/components/Tabs.js
@@ -1,42 +1,34 @@
 import React, { PureComponent } from 'react'
 import { connect } from 'react-redux'
-import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import { NavLink } from 'react-router-dom'
 
 import colors from '~/constants/colors'
 
-class Tabs extends PureComponent {
-   static propTypes = {
-      theme: PropTypes.string.isRequired
-   }
-
+export default class Tabs extends PureComponent {
    render() {
-      const { theme } = this.props
       return (
          <TabsWrapper>
-            <Tab theme={theme} to="/" exact>
+            <Tab to="/" exact>
                Welcome
             </Tab>
-            <Tab theme={theme} to="/worlds">
-               Worlds
-            </Tab>
+            <Tab to="/worlds">Worlds</Tab>
          </TabsWrapper>
       )
    }
 }
 
-const mapStateToProps = state => ({ theme: state.settings.theme })
-export default connect(mapStateToProps)(Tabs)
-
-const TabsWrapper = styled.div`
+const TabsWrapper = styled.ul`
    position: absolute;
    bottom: 0;
    left: 0;
    margin: 0 15px;
+   padding: 0;
 `
 
-const Tab = styled(NavLink)`
+const NavLinkWarningFix = ({ dispatch, ...rest }) => <NavLink {...rest} />
+
+const Tab = connect(state => ({ theme: state.settings.theme }))(styled(NavLinkWarningFix)`
    font-size: 24px;
    border-right: solid 1px ${({ theme }) => colors[theme].DIVIDER_STRONG};
    padding: 0 13px;
@@ -53,4 +45,4 @@ const Tab = styled(NavLink)`
    :last-child {
       border-right: none;
    }
-`
+`)
