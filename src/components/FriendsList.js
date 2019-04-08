c464b8640bc94fb518d5c30d630e108ce52aaa52
Kinark <igormarcossi@gmail.com>
2019-04-07 21:56:37 -0300

    Added FriendsList (unfinished/base) component

---
+++ src/components/FriendsList.js
@@ -0,0 +1,21 @@
+import React, { PureComponent } from 'react'
+// import styled from 'styled-components'
+
+import Sidebar from '~/components/Sidebar'
+
+export default class index extends PureComponent {
+   render() {
+      return (
+         <Sidebar>
+            Friendslist
+         </Sidebar>
+      )
+   }
+}
+
+// const Nav = styled.nav`
+//    display: flex;
+//    justify-content: space-between;
+//    padding: 15px 20px 50px;
+//    position: relative;
+// `
