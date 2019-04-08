96ae00a15a19d4461d25066292d721f45199ce85
Kinark <igormarcossi@gmail.com>
2019-04-08 15:55:30 -0300

    Styled Sidebar component

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,21 +1,43 @@
 import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
+import { connect } from 'react-redux'
+
+import CustomScroll from '~/components/CustomScroll'
+import SectionTitle from '~/components/SectionTitle'
+import TitleInfo from '~/components/TitleInfo'
 
 import colors from '~/constants/colors'
 
-export default class index extends PureComponent {
+export default class Sidebar extends PureComponent {
    static propTypes = {
-      children: PropTypes.node.isRequired
+      children: PropTypes.node.isRequired,
+      align: PropTypes.oneOf(['left', 'right']).isRequired,
+      title: PropTypes.string.isRequired,
+      titleInfo: PropTypes.string.isRequired
    }
 
    render() {
-      const { children } = this.props
-      return <SideNav>{children}</SideNav>
+      const { children, title, titleInfo, align } = this.props
+      return (
+         <SideNav>
+            <TitleSection className={`${align}-align`}>
+               <SectionTitle>{title}</SectionTitle>
+               <TitleInfo>{titleInfo}</TitleInfo>
+            </TitleSection>
+            <CustomScroll>{children}</CustomScroll>
+         </SideNav>
+      )
    }
 }
 
-const SideNav = styled.aside`
+const SideNav = connect(state => ({ theme: state.settings.theme }))(styled.aside`
    height: 100%;
+   width: 310px;
+   padding: 30px 30px 0;
    background-color: ${({ theme }) => colors[theme].SECTION_1};
+`)
+
+const TitleSection = styled.div`
+ margin-bottom: 20px;  
 `
