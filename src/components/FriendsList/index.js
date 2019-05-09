60301db25b0e714a929369567695a1ec51d31a9a
Kinark <igormarcossi@gmail.com>
2019-05-09 13:34:06 -0300

    Adding Friend component import

--- src/components/FriendsList/index.js
+++ src/components/FriendsList/index.js
@@ -2,6 +2,7 @@ import React, { PureComponent } from 'react'
 // import styled from 'styled-components'
 
 import Sidebar from '~/components/Sidebar'
+import Friend from './components/Friend'
 
 export default class FriendsList extends PureComponent {
    render() {
