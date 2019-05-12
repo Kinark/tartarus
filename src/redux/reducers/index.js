5a9b3e09a71ae36ddaa9e414a1119945ce04868a
Kinark <igormarcossi@gmail.com>
2019-05-12 01:39:29 -0300

    Added data reducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
 import settings from './settings'
 import app from './app'
+import data from './data'
 
 export default combineReducers({
    settings,
+   data,
    app
 })
