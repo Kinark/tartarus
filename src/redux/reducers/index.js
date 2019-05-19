374230182df01576d3135b807a05c407f0e0177f
Kinark <igormarcossi@gmail.com>
2019-05-19 03:36:36 -0300

    Fixed absence of newWorld reducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -3,10 +3,12 @@ import settings from './settings'
 import app from './app'
 import data from './data'
 import auth from './auth'
+import newWorld from './newWorld'
 import myWorlds from './myWorlds'
 
 export default combineReducers({
    myWorlds,
+   newWorld,
    settings,
    data,
    auth,
