9883caf8f0db08f18149a72289aedfbe894326ea
Kinark <igormarcossi@gmail.com>
2019-05-20 14:29:40 -0300

    WorldLink component created (unfinished)

---
+++ src/components/WorldLink.js
@@ -0,0 +1,55 @@
+import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+
+import colors from '~/constants/colors'
+
+export default class WorldLink extends PureComponent {
+   static propTypes = {
+      data: PropTypes.shape({
+         _id: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+        description: PropTypes.string,
+        members: PropTypes.arrayOf(PropTypes.string).isRequired,
+        ruleset: PropTypes.string,
+      }).isRequired,
+   }
+
+   render() {
+      const { data } = this.props
+      return (
+         <World to={`/world/${data._id}`}>
+            <WorldTitle>{data.name}</WorldTitle>
+            {!!data.description && <div>{data.description}</div>}
+            <WorldMembers>
+               <div>{data.members.length} MEMBROS</div>
+               {!!data.ruleset && <div>{data.ruleset}</div>}
+            </WorldMembers>
+         </World>
+      )
+   }
+}
+
+const World = styled(Link)`
+   border-radius: 8px;
+   border: ${({ theme, covered }) => (covered ? 'none' : `solid 1px ${theme.TITLE}`)};
+   color: ${({ theme, covered }) => (covered ? colors.dark.TITLE : theme.TITLE)};
+   height: 75px;
+   display: flex;
+   justify-content: space-between;
+   flex-direction: column;
+   margin: 0 12px 12px 0;
+   padding: 8px 10px;
+`
+
+const WorldTitle = styled.div`
+   font-family: 'upgrade', sans-serif;
+   font-weight: 500;
+`
+
+const WorldMembers = styled.div`
+   font-size: 11px;
+   text-transform: uppercase;
+   display: flex;
+`
