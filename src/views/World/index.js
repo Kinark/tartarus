90750a8111e564511653d4a8fa7c83059634c6f8
Kinark <igormarcossi@gmail.com>
2019-05-21 13:00:19 -0300

    Add tab on enter room link

--- src/views/World/index.js
+++ src/views/World/index.js
@@ -1,13 +1,24 @@
 import React, { PureComponent } from 'react'
+import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 
-export default class World extends PureComponent {
+import { enterRoomAndAddTab } from '~/redux/actions/app'
+
+class World extends PureComponent {
    static propTypes = {
+      dispatch: PropTypes.func.isRequired,
+      openedTabs: PropTypes.arrayOf(PropTypes.string).isRequired,
       match: PropTypes.shape({
          params: PropTypes.shape({
             worldId: PropTypes.string.isRequired
          }).isRequired
       }).isRequired
+   }
+
+   componentDidMount = () => {
+      const { openedTabs, dispatch, match } = this.props
+      const { worldId } = match.params
+      if (!openedTabs.find(world => world !== worldId)) dispatch(enterRoomAndAddTab(worldId))
    }
 
    render() {
@@ -16,3 +27,5 @@ export default class World extends PureComponent {
       return <div>Você está no mundo {worldId}</div>
    }
 }
+const mapStateToProps = state => ({ openedTabs: state.app.tabs })
+export default connect(mapStateToProps)(World)
