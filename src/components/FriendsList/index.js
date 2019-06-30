178cca8bbbdbab1affbe1ef84166e7c9d0d9897d
Kinark <igormarcossi@gmail.com>
2019-06-30 04:19:07 -0300

    Text

--- src/components/FriendsList/index.js
+++ src/components/FriendsList/index.js
@@ -7,7 +7,7 @@ import Friend from './components/Friend'
 export default class FriendsList extends PureComponent {
    render() {
       return (
-         <Sidebar align="left" title="Lista de amigos" titleInfo="Lute com animops">
+         <Sidebar align="left" title="Lista de amigos" titleInfo="Fale com seus companheiros">
             Friendslist
          </Sidebar>
       )
