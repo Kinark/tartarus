9fe41d835c24117eb5e7e29f6a7cbab947f8685a
Kinark <igormarcossi@gmail.com>
2019-06-29 23:22:03 -0300

    Div -> Section html tag

--- src/components/SectionContent.js
+++ src/components/SectionContent.js
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 import colors from '~/constants/colors'
 
-const SectionContent = styled.div`
+const SectionContent = styled.section`
    padding: 30px 30px 0;
    height: 100%;
    border-right: ${({ bordered, theme }) => (bordered ? `solid 1px ${theme.DIVIER_SOFT}` : 'none')};
