9981b0f64dacdf55b0077c4242a72a5acde013cf
Kinark <igormarcossi@gmail.com>
2019-07-05 14:42:52 -0300

    Reconnection settings changed

--- src/instances/socket.js
+++ src/instances/socket.js
@@ -1,4 +1,8 @@
 import io from 'socket.io-client'
 import apiUrl from '~/constants/apiUrl'
 
-export default io(apiUrl)
+export default io.connect(apiUrl, {
+   reconnection: true,
+   reconnectionDelay: 1000,
+   reconnectionAttempts: 100
+ })
