db67976d01611e05f1a8953e1ad120ab8e693a2b
Kinark <igormarcossi@gmail.com>
2019-07-09 16:43:00 -0300

    Added special baloon for rolls

--- src/components/Chat/components/Message.js
+++ src/components/Chat/components/Message.js
@@ -23,7 +23,22 @@ class Message extends PureComponent {
             </AvatarColumn>
             <ContentColumn owned={data.author._id === myId}>
                <Author>{data.author.name}</Author>
-               <Content owned={data.author._id === myId}>{data.content}</Content>
+               {data.dicesResults && data.dicesResults.length > 0 ? (
+                  <ContentRoll owned={data.author._id === myId}>
+                     <RollResult>{data.content}</RollResult>
+                     <RollDices>
+                        {data.dices.map((dice, i) => (
+                           <EachDice key={i}>
+                              <span>{dice}</span>
+                              <br />
+                              {data.dicesResults[i]}
+                           </EachDice>
+                        ))}
+                     </RollDices>
+                  </ContentRoll>
+               ) : (
+                  <Content owned={data.author._id === myId}>{data.content}</Content>
+               )}
             </ContentColumn>
          </MessageWrapper>
       )
@@ -35,15 +50,15 @@ export default connect(mapStateToProps)(Message)
 const MessageWrapper = styled.div`
    display: flex;
    margin: 20px 0;
-   opacity: ${({ loading }) => loading ? '.5' : '1'};
+   opacity: ${({ loading }) => (loading ? '.5' : '1')};
    transition: opacity 300ms;
 `
 
 const AvatarColumn = styled.div`
    flex: 0;
-   margin-right: ${({ owned }) => owned ? '0' : '10px'};
-   margin-left: ${({ owned }) => owned ? '10px' : '0'};
-   order: ${({ owned }) => owned ? '1' : '0'};
+   margin-right: ${({ owned }) => (owned ? '0' : '10px')};
+   margin-left: ${({ owned }) => (owned ? '10px' : '0')};
+   order: ${({ owned }) => (owned ? '1' : '0')};
 `
 
 const Avatar = styled.img`
@@ -54,7 +69,7 @@ const Avatar = styled.img`
 
 const ContentColumn = styled.div`
    flex: 1;
-   text-align: ${({ owned }) => owned ? 'right' : 'left'};
+   text-align: ${({ owned }) => (owned ? 'right' : 'left')};
 `
 
 const Author = styled.div`
@@ -64,7 +79,7 @@ const Author = styled.div`
 
 const Content = styled.div`
    flex: 1;
-   border-radius: ${({ owned }) => owned ? '15px 0' : '0 15px'} 15px 15px;
+   border-radius: ${({ owned }) => (owned ? '15px 0' : '0 15px')} 15px 15px;
    background-color: ${({ theme }) => theme.CHAT_BG_COLOR};
    padding: 13px 16px 16px;
    max-width: 75%;
@@ -72,3 +87,43 @@ const Content = styled.div`
    text-align: left;
 `
 ContentColumn.defaultProps = { theme: colors.light }
+
+const ContentRoll = styled.div`
+   flex: 1;
+   border-radius: ${({ owned }) => (owned ? '15px 0' : '0 15px')} 15px 15px;
+   background-color: ${({ theme }) => theme.CHAT_BG_COLOR};
+   border: solid 3px ${({ theme }) => theme.TITLE};
+   /* padding: 13px 16px 16px; */
+   max-width: 75%;
+   display: inline-flex;
+   text-align: left;
+`
+ContentColumn.defaultProps = { theme: colors.light }
+
+const RollResult = styled.div`
+   flex: 0;
+   border-radius: 0 0 ${({ owned }) => (owned ? '0 7px' : '7px 0')};
+   background-color: ${({ theme }) => theme.TITLE};
+   padding: 3px 16px;
+   font-family: 'upgrade',sans-serif;
+   font-size: 32px;
+   font-weight: 500;
+   color: ${({ theme }) => theme.BG};
+   order: ${({ owned }) => (owned ? '0' : '1')};
+`
+RollResult.defaultProps = { theme: colors.light }
+
+const RollDices = styled.div`
+   flex: 1;
+   display: flex;
+   flex-direction: row;
+   padding: 7px 16px;
+   line-height: normal;
+`
+
+const EachDice = styled.div`
+   margin: 0 5px;
+   span {
+      font-weight: 700;
+   }
+`
