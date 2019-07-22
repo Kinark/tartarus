0bf756312a67c1bbb1d750bb54b00f887049f6cf
Kinark <igormarcossi@gmail.com>
2019-07-22 19:32:10 -0300

    Prettier

--- src/components/Draggable.js
+++ src/components/Draggable.js
@@ -1,111 +1,114 @@
-import React from 'react';
-import styled, { css } from 'styled-components';
+import React from 'react'
+import styled, { css } from 'styled-components'
 
 export default class Draggable extends React.Component {
-  state = {
-    isDragging: false,
+   state = {
+      isDragging: false,
 
-    originalX: 0,
-    originalY: 0,
+      originalX: 0,
+      originalY: 0,
 
-    translateX: 0,
-    translateY: 0,
+      translateX: 0,
+      translateY: 0,
 
-    lastTranslateX: 0,
-    lastTranslateY: 0
-  };
+      lastTranslateX: 0,
+      lastTranslateY: 0
+   }
 
-  componentWillUnmount() {
-    window.removeEventListener('mousemove', this.handleMouseMove);
-    window.removeEventListener('mouseup', this.handleMouseUp);
-  }
+   componentWillUnmount() {
+      window.removeEventListener('mousemove', this.handleMouseMove)
+      window.removeEventListener('mouseup', this.handleMouseUp)
+   }
 
-  handleMouseDown = ({ clientX, clientY }) => {
-    window.addEventListener('mousemove', this.handleMouseMove);
-    window.addEventListener('mouseup', this.handleMouseUp);
+   handleMouseDown = ({ clientX, clientY }) => {
+      window.addEventListener('mousemove', this.handleMouseMove)
+      window.addEventListener('mouseup', this.handleMouseUp)
 
-    if (this.props.onDragStart) {
-      this.props.onDragStart();
-    }
-
-    this.setState({
-      originalX: clientX,
-      originalY: clientY,
-      isDragging: true
-    });
-  };
-
-  handleMouseMove = ({ clientX, clientY }) => {
-    const { isDragging } = this.state;
-    const { onDrag } = this.props;
-
-    if (!isDragging) {
-      return;
-    }
-
-    this.setState(prevState => ({
-      translateX: clientX - prevState.originalX + prevState.lastTranslateX,
-      translateY: clientY - prevState.originalY + prevState.lastTranslateY
-    }), () => {
-      if (onDrag) {
-        onDrag({
-          translateX: this.state.translateX,
-          translateY: this.state.translateY
-        });
+      if (this.props.onDragStart) {
+         this.props.onDragStart()
       }
-    });
-  };
 
-  handleMouseUp = () => {
-    window.removeEventListener('mousemove', this.handleMouseMove);
-    window.removeEventListener('mouseup', this.handleMouseUp);
+      this.setState({
+         originalX: clientX,
+         originalY: clientY,
+         isDragging: true
+      })
+   }
 
-    this.setState(
-      {
-        originalX: 0,
-        originalY: 0,
-        lastTranslateX: this.state.translateX,
-        lastTranslateY: this.state.translateY,
+   handleMouseMove = ({ clientX, clientY }) => {
+      const { isDragging } = this.state
+      const { onDrag } = this.props
 
-        isDragging: false
-      },
-      () => {
-        if (this.props.onDragEnd) {
-          this.props.onDragEnd();
-        }
+      if (!isDragging) {
+         return
       }
-    );
-  };
 
-  render() {
-    const { children } = this.props;
-    const { translateX, translateY, isDragging } = this.state;
+      this.setState(
+         prevState => ({
+            translateX: clientX - prevState.originalX + prevState.lastTranslateX,
+            translateY: clientY - prevState.originalY + prevState.lastTranslateY
+         }),
+         () => {
+            if (onDrag) {
+               onDrag({
+                  translateX: this.state.translateX,
+                  translateY: this.state.translateY
+               })
+            }
+         }
+      )
+   }
 
-    return (
-      <Container
-        onMouseDown={this.handleMouseDown}
-        x={translateX}
-        y={translateY}
-        isDragging={isDragging}
-      >
-        {children}
-      </Container>
-    );
-  }
+   handleMouseUp = () => {
+      window.removeEventListener('mousemove', this.handleMouseMove)
+      window.removeEventListener('mouseup', this.handleMouseUp)
+
+      this.setState(
+         {
+            translateX: 0,
+            translateY: 0,
+            originalX: 0,
+            originalY: 0,
+            //   lastTranslateX: this.state.translateX,
+            //   lastTranslateY: this.state.translateY,
+            lastTranslateX: 0,
+            lastTranslateY: 0,
+
+            isDragging: false
+         },
+         () => {
+            if (this.props.onDragEnd) {
+               this.props.onDragEnd()
+            }
+         }
+      )
+   }
+
+   render() {
+      const { children } = this.props
+      const { translateX, translateY, isDragging } = this.state
+
+      return (
+         <Container onMouseDown={this.handleMouseDown} x={translateX} y={translateY} isDragging={isDragging}>
+            {children}
+         </Container>
+      )
+   }
 }
 
 const Container = styled.div.attrs({
-  style: ({ x, y }) => ({
-    transform: `translate(${x}px, ${y}px)`
-  }),
+   style: ({ x, y }) => ({
+      transform: `translate(${x}px, ${y}px)`
+   })
 })`
-  cursor: grab;
-  
-  ${({ isDragging }) =>
-  isDragging && css`
-    opacity: 0.8;
-    cursor: grabbing;
-    position: fixed;
-    z-index: 999999;
-  `};
-`;
+   cursor: grab;
+
+   ${({ isDragging }) =>
+      isDragging &&
+      css`
+         opacity: 0.8;
+         cursor: grabbing;
+         position: fixed;
+         z-index: 999999;
+      `};
+`
