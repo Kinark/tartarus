6c8d9caa1a097c0ac4f9700c3bbd57c0aa3bf36a
Kinark <igormarcossi@gmail.com>
2019-07-25 02:47:14 -0300

    CustomContext base

--- src/components/PlayersList/index.js
+++ src/components/PlayersList/index.js
@@ -1,13 +1,17 @@
 import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
+import { MenuItem, ContextMenuTrigger } from 'react-contextmenu'
 // import styled from 'styled-components'
 
 import playerPropTypes from '~/propTypes/player'
 
+import CustomContext from '~/components/CustomContext'
 import Sidebar from '~/components/Sidebar'
 
 import Player from './components/Player'
+
+const collect = props => props
 
 class PlayersList extends PureComponent {
    static propTypes = {
@@ -16,19 +20,50 @@ class PlayersList extends PureComponent {
             members: PropTypes.arrayOf(PropTypes.shape(playerPropTypes)).isRequired
          })
       ).isRequired,
+      myId: PropTypes.string.isRequired,
       room: PropTypes.string.isRequired
    }
 
+   giveSheet = (e, data) => {
+      console.log(data)
+   }
+
    render() {
-      const { tabs, room } = this.props
+      const { tabs, myId, room } = this.props
+      const roomObj = tabs.find(tab => tab._id === room)
+      const isOwner = roomObj ? roomObj.owner === myId : false
       return (
          <Sidebar align="left" title="Lista de jogadores" titleInfo="Fale com seus companheiros">
-            {tabs.find(tab => tab._id === room) ? (
+            {tabs.length ? (
                <React.Fragment>
                   <div>Online:</div>
-                  {tabs.find(tab => tab._id === room).members.map(player => (player.online ? <Player key={player._id} data={player} /> : ''))}
+                  {roomObj.members
+                     .filter(member => member.online)
+                     .map(player => (
+                        <ContextMenuTrigger key={player._id} id="playerMenu" playerId={player._id} collect={collect}>
+                           <Player data={player} />
+                        </ContextMenuTrigger>
+                     ))}
                   <div>Offline:</div>
-                  {tabs.find(tab => tab._id === room).members.map(player => (!player.online ? <Player key={player._id} data={player} /> : ''))}
+                  {roomObj.members
+                     .filter(member => !member.online)
+                     .map(player => (
+                        <Player key={player._id} data={player} />
+                     ))}
+                  <CustomContext id="playerMenu">
+                     {isOwner ? (
+                        <React.Fragment>
+                           <MenuItem onClick={this.giveSheet} data={{ action: 'Added' }}>
+                              Dar ficha para jogador
+                           </MenuItem>
+                        </React.Fragment>
+                     ) : (
+                        <React.Fragment>
+                           <MenuItem data={{ action: 'Removed' }}>Opção para quem não é dono</MenuItem>
+                        </React.Fragment>
+                     )}
+                     <MenuItem data={{ action: 'Removed' }}>Ver ficha</MenuItem>
+                  </CustomContext>
                </React.Fragment>
             ) : (
                'Loading...'
@@ -37,5 +72,5 @@ class PlayersList extends PureComponent {
       )
    }
 }
-const mapStateToProps = state => ({ tabs: state.app.tabs })
+const mapStateToProps = state => ({ tabs: state.app.tabs, myId: state.auth.userId })
 export default connect(mapStateToProps)(PlayersList)
