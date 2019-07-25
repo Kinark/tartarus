77733c53ef12e1afc952bad274c341600c28c4e8
Kinark <igormarcossi@gmail.com>
2019-07-25 19:44:32 -0300

    Added World -> Sheet component

---
+++ src/views/World/components/Sheet.js
@@ -0,0 +1,11 @@
+import React, { PureComponent } from 'react'
+
+export default class Sheet extends PureComponent {
+   render() {
+      return (
+         <div>
+            Ficha
+         </div>
+      )
+   }
+}
