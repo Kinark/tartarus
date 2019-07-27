3424d3d22820ac22fadb1b0b70c7e03948bbbba8
Kinark <igormarcossi@gmail.com>
2019-07-27 13:18:11 -0300

    Fixed some CSS issues

--- src/views/World/index.js
+++ src/views/World/index.js
@@ -61,18 +61,18 @@ class World extends PureComponent {
                   path={match.path}
                   exact
                   component={() => (
-                     <div className="row no-mrg">
+                     <StyledRow className="row no-mrg">
                         <FullHeight className="col xs12 m6">
                            <SectionContent className="no-pad" bordered>
                               <Chat data={adventureMessages} title="Aventura" room={worldId} type="adventure" />
                            </SectionContent>
                         </FullHeight>
                         <FullHeight className="col xs12 m6">
-                           <SectionContent className="no-pad" bordered>
+                           <SectionContent className="no-pad">
                               <Chat data={chatMessages} title="Conversa" room={worldId} type="talk" />
                            </SectionContent>
                         </FullHeight>
-                     </div>
+                     </StyledRow>
                   )}
                />
             </StyledAppMainWrapper>
@@ -86,4 +86,8 @@ export default connect(mapStateToProps)(World)
 const StyledAppMainWrapper = styled(AppMainWrapper)`
    display: flex;
    flex-direction: column;
+`
+
+const StyledRow = styled.div`
+   flex: 1 !important;
 `
