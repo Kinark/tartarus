21f674b5494652d0b18994ba02025b96d347c552
Kinark <igormarcossi@gmail.com>
2019-07-27 13:17:43 -0300

    Removed required from players property

--- src/propTypes/player.js
+++ src/propTypes/player.js
@@ -4,7 +4,7 @@ const playerPropTypes = {
    _id: PropTypes.string.isRequired,
    online: PropTypes.bool.isRequired,
    user: PropTypes.shape({
-      createdAt: PropTypes.string.isRequired,
+      createdAt: PropTypes.string,
       currentSocket: PropTypes.string,
       name: PropTypes.string.isRequired,
       _id: PropTypes.string.isRequired
