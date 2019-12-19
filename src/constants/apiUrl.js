f28ef5d106156ea7adf94912d5be5b8efd330ddc
Kinark <igormarcossi@gmail.com>
2019-12-19 19:14:43 -0300

    Changed default apiUrl

--- src/constants/apiUrl.js
+++ src/constants/apiUrl.js
@@ -1,1 +1,1 @@
-export default (process.env.NODE_ENV === 'development' ? '//192.168.15.250:3000' : '//api.tartarusrpg.com')
+export default (process.env.NODE_ENV === 'development' ? '//192.168.15.40:3000' : '//api.tartarusrpg.com')
